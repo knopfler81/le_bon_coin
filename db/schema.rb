f75ce079227f4759257e7330124fe3534a478cd1
Nelly Duclos <knopfler42@yahoo.fr>
2017-04-19 13:03:13 +0200

    added routes and added address and location to adverts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419104112) do
+ActiveRecord::Schema.define(version: 20170419110123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170419104112) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "location"
+    t.string   "address"
     t.index ["user_id"], name: "index_adverts_on_user_id", using: :btree
   end
 
